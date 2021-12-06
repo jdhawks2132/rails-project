90e05f4b7def021d0e98eab2a12b6c3513dcaa81
Joshua <jdhawks@gmail.com>
2021-12-05 22:16:39 -0600

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+	Blog.create(title: Faker::Book.title, body: Faker::GreekPhilosophers.quote)
+end
+
+Language.create(title: 'Javascript', percent_used: 50)
+Language.create(title: 'Ruby on Rails', percent_used: 30)
+Language.create(title: 'SQL', percent_used: 10)
+Language.create(title: 'HTML/CSS', percent_used: 10)
+
+9.times do
+	Portfolio.create(
+		title: Faker::Lorem.word,
+		subtitle: Faker::Lorem.sentence(word_count: 3),
+		body: Faker::Lorem.paragraph(sentence_count: 2),
+		main_image: 'https://placehold.co/600x400',
+		thumb_image: 'https://placehold.co/350x200',
+	)
+end
