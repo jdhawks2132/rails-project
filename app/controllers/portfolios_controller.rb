6a447e2037673376f0bc764fcc2f082ddfd46b7b
Joshua <jdhawks@gmail.com>
2021-12-11 11:49:55 -0600

    added new/ update

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,27 +8,48 @@ class PortfoliosController < ApplicationController
 	def show; end
 
 	def new
-		@portfolio = Portfolio.new
+		@portfolio_item = Portfolio.new
 	end
 
 	# POST /blogs or /blogs.json
 	def create
-		@portfolio = Portfolio.new(portfolio_params)
+		@portfolio_item = Portfolio.new(portfolio_params)
 
 		respond_to do |format|
-			if @portfolio.save
+			if @portfolio_item.save
 				format.html do
-					redirect_to @portfolio, notice: 'portfolio was successfully created.'
+					redirect_to portfolios_path,
+					            notice: 'portfolio was successfully created.'
 				end
-				format.json { render :show, status: :created, location: @portfolio }
+				format.json do
+					render :show, status: :created, location: @portfolio_item
+				end
 			else
 				format.html { render :new, status: :unprocessable_entity }
 				format.json do
-					render json: @portfolio.errors, status: :unprocessable_entity
+					render json: @portfolio_item.errors, status: :unprocessable_entity
 				end
 			end
 		end
 	end
+
+	def edit
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+
+	def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+		respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: "Blog was successfully updated." }
+        format.json { render :show, status: :ok, location: @portfolio_item }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 	private
 
@@ -39,6 +60,6 @@ class PortfoliosController < ApplicationController
 
 	# Only allow a list of trusted parameters through.
 	def portfolio_params
-		params.require(:portfolio).permit(:title, :subititle, :description)
+		params.require(:portfolio).permit(:title, :subtitle, :body)
 	end
 end
